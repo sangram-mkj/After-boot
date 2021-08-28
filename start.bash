5bbc62dc0864d3a53b6439558c9ba891f558883b
sangram-mkj <89677356+sangram-mkj@users.noreply.github.com>
2021-08-28 04:42:31 +0000

    Add files via upload

---
+++ start.bash
@@ -0,0 +1,25 @@
+sudo apt updade -y
+sudo apt upgrade -y
+sudo apt install neofetch -y
+sudo apt-get install snapd -y
+sudo snap install discord -y
+sudo apt install telegram-desktop -y
+sudo apt install firefox -y
+
+sudo apt install gdebi -y
+sudo apt install gparted -y
+
+sudo snap install snap-store
+sudo apt install vlc -y
+sudo apt install wget -y
+
+sudo apt update
+sudo snap install --classic code
+
+wget https://download.bluemail.me/BlueMail/deb/BlueMail.deb
+sudo apt install curl -y
+sudo apt install ./BlueMail.deb -y
+
+sudo snap install wps-office 
+sudo apt install git
+sudo apt update
